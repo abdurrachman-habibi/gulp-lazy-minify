fe8e0783afe422327f146c423eecee54fb06ae27
Habibi <guilvee@gmail.com>
2015-09-13 18:18:50 +0800

    Fix main function not called correctly

--- index.js
+++ index.js
@@ -18,52 +18,51 @@ var fn = function (fileType) {
     return true;
 };
 
-var lazyMinify = through.obj(function (file, enc, callback) {
-    var subs = file.path.toLowerCase().split('.');
-    var isMinified = subs.length > 2 && subs[subs.length - 2] === 'min';
+var lazyMinify = function() {
+    var stream = through.obj(function (file, enc, callback) {
+        var subs = file.path.toLowerCase().split('.');
+        var isMinified = subs.length > 2 && subs[subs.length - 2] === 'min';
 
-    if (isMinified) {
-        console.log('source-minified: ' + file.path);
-        callback(null, file);
-    }
-    else {
-        var fileType = subs[subs.length - 1];
-        var minPath = subs[0];
-        for (var i = 0; i < subs.length - 2; i++) {
-            minPath += '.' + subs[i];
+        if (isMinified) {
+            //console.log('source-minified: ' + file.path);
+            callback(null, file);
         }
-        minPath += '.min.' + fileType;
-
-        var basename = path.basename(file.path);
-        var filename = basename.substring(0, basename.lastIndexOf('.')) + '.min.' + fileType;
-
-        fs.readFile(minPath, function (err, data) {
-            if (err == null) {
-                console.log('pre-minified: ' + minPath);
-
-                var minFile = new File({
-                    path: filename,
-                    contents: data
-                });
-
-                callback(null, minFile);
-            } else {
-                console.log('to-minify: ' + file.path);
-
-
-                gulpFile(filename, file.contents, { src: true })
-                    .pipe(fn(fileType))
-                    .pipe(through.obj(function (minFile, enc, cb) {
-                        cb();
-                        callback(null, minFile);
-                    }));
+        else {
+            var fileType = subs[subs.length - 1];
+            var minPath = subs[0];
+            for (var i = 0; i < subs.length - 2; i++) {
+                minPath += '.' + subs[i];
             }
-        });
-    }
-});
+            minPath += '.min.' + fileType;
 
-module.exports = function() {
-    return lazyMinify;
+            var basename = path.basename(file.path);
+            var filename = basename.substring(0, basename.lastIndexOf('.')) + '.min.' + fileType;
+
+            fs.readFile(minPath, function (err, data) {
+                if (err == null) {
+                    //console.log('pre-minified: ' + minPath);
+
+                    var minFile = new File({
+                        path: filename,
+                        contents: data
+                    });
+
+                    callback(null, minFile);
+                } else {
+                    //console.log('to-minify: ' + file.path);
+
+                    gulpFile(filename, file.contents, { src: true })
+                        .pipe(fn(fileType))
+                        .pipe(through.obj(function (minFile, enc, cb) {
+                            cb();
+                            callback(null, minFile);
+                        }));
+                }
+            });
+        }
+    });
+
+    return stream;
 };
 
-
+module.exports = lazyMinify;
